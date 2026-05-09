48bb9c215080e3616367ecacdc91a1d9ce4926d8
FedericoQuartieri <federico.quartieri2002@gmail.com>
2026-05-09 17:10:24 +0200

    added Syntesis ipynb

--- 2026/syntesis_1_2.py
+++ 2026/syntesis_1_2.py
@@ -1,4 +1,38 @@
+from typing import Any
+
+import numpy as np
 from bloqade import squin
+from bloqade.types import Qubit
+from kirin.dialects.ilist import IList
+
+Register = IList[Qubit, Any]
+
+def Rz(theta):
+    return np.array([
+        [np.exp(-1j * theta / 2), 0],
+        [0, np.exp(1j * theta / 2)]
+    ], dtype=complex)
+
+
+I = np.eye(2, dtype=complex)
+
+H = 1 / np.sqrt(2) * np.array([
+    [1, 1],
+    [1, -1]
+], dtype=complex)
+
+S = np.array([
+    [1, 0],
+    [0, 1j]
+], dtype=complex)
+
+T = np.array([
+    [1, 0],
+    [0, np.exp(1j * np.pi / 4)]
+], dtype=complex)
+
+def gate_distance(U, V):
+    return np.sqrt(1 - abs(np.trace(U.conj().T @ V)) / 2)
 
 @squin.kernel
 def X_gate(qubit) -> Register:
@@ -73,9 +107,9 @@ def Toffoli_gate(qubit1, qubit2, qubit3) -> Register:
 
 @squin.kernel
 def CCZ_gate(qubit1, qubit2, qubit3) -> Register:
-    squin.H(qubit3)
-    Toffoli_gate(qubit1, qubit2)
-    squin.H(qubit3)
+    squin.h(qubit3)
+    Toffoli_gate(qubit1, qubit2, qubit3)
+    squin.h(qubit3)
     return [qubit1, qubit2, qubit3]
 
 
@@ -84,15 +118,16 @@ def Rz_gate(qubit, n) -> Register:
     if (n == 0):
         qubit = X_gate(qubit)
     elif (n == 1):
-        squin.S(qubit)
+        squin.s(qubit)
     elif (n == 2):
-        squin.T(qubit)
-    elif (n == 3):
-        pass
-    elif (n == 4):
-        pass
-    elif (n == 5):
-        pass
-
-
+        squin.t(qubit)
     return qubit
+
+
+@squin.kernel
+def Rx_gate(qubit, n) -> Register:
+    squin.h(qubit)
+    Rz_gate(qubit, n)
+    squin.h(qubit)
+    return qubit
+
